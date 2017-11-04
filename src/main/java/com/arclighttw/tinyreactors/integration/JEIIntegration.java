374bf88390583f74093721166e4e2ed0ab71e6ec
Tom <tomawright95@gmail.com>
2017-11-04 16:27:43 +0000

    JEI fix

--- src/main/java/com/arclighttw/tinyreactors/integration/JEIIntegration.java
+++ src/main/java/com/arclighttw/tinyreactors/integration/JEIIntegration.java
@@ -5,7 +5,7 @@ import com.arclighttw.tinyreactors.inits.TRBlocks;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 @JEIPlugin
 public class JEIIntegration implements IModPlugin
@@ -13,7 +13,6 @@ public class JEIIntegration implements IModPlugin
 	@Override
 	public void register(IModRegistry registry)
 	{
-		registry.getJeiHelpers().getIngredientBlacklist().addIngredientToBlacklist(TRBlocks.DEGRADED_REACTANT);
-		registry.getJeiHelpers().getIngredientBlacklist().addIngredientToBlacklist(Item.getItemFromBlock(TRBlocks.DEGRADED_REACTANT));
+		registry.getJeiHelpers().getIngredientBlacklist().addIngredientToBlacklist(new ItemStack(TRBlocks.DEGRADED_REACTANT));
 	}
 }
