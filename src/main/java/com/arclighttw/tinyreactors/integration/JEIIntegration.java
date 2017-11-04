922535cea1026618908312fe49f6f8d70488d644
Tom <tomawright95@gmail.com>
2017-11-04 22:28:22 +0000

    More JEI work

--- src/main/java/com/arclighttw/tinyreactors/integration/JEIIntegration.java
+++ src/main/java/com/arclighttw/tinyreactors/integration/JEIIntegration.java
@@ -11,8 +11,11 @@ import net.minecraft.item.ItemStack;
 public class JEIIntegration implements IModPlugin
 {
 	@Override
+	@SuppressWarnings("deprecation")
 	public void register(IModRegistry registry)
 	{
 		registry.getJeiHelpers().getIngredientBlacklist().addIngredientToBlacklist(new ItemStack(TRBlocks.DEGRADED_REACTANT));
+		
+		registry.addDescription(new ItemStack(TRBlocks.REACTOR_ENERGY_PORT), "Combine Energy Ports to accumulate production.", "e.g. 2x Energy Ports with Limit of 256 RF/t will craft to 1x Energy Port with Limit of 512 RF/t.");
 	}
 }
