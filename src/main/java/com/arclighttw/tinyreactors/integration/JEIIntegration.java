d8cd04991fe825617ac80f8ffbd07b3fb7d699a6
Tom <tomawright95@gmail.com>
2017-11-06 13:20:52 +0000

    Updated JEI to reflect new Capacitor system

--- src/main/java/com/arclighttw/tinyreactors/integration/JEIIntegration.java
+++ src/main/java/com/arclighttw/tinyreactors/integration/JEIIntegration.java
@@ -17,5 +17,6 @@ public class JEIIntegration implements IModPlugin
 		registry.getJeiHelpers().getIngredientBlacklist().addIngredientToBlacklist(new ItemStack(TRBlocks.DEGRADED_REACTANT));
 		
 		registry.addDescription(new ItemStack(TRBlocks.REACTOR_ENERGY_PORT), "Combine Energy Ports to accumulate production.", "e.g. 2x Energy Ports with Limit of 256 RF/t will craft to 1x Energy Port with Limit of 512 RF/t.");
+		registry.addDescription(new ItemStack(TRBlocks.CAPACITOR), "Combine Capacitors to accumulate production.", "e.g. 2x Capacitors with Limit of 256 RF/t will craft to 1x Capacitor with Limit of 512 RF/t.");
 	}
 }
