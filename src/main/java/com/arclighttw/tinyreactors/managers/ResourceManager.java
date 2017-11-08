05802889ea3c3b6e6c4ec8d8d0862a35f42bd35e
Tom <tomawright95@gmail.com>
2017-11-08 07:57:12 +0000

    Can verify if resource package and/or file exists

--- src/main/java/com/arclighttw/tinyreactors/managers/ResourceManager.java
+++ src/main/java/com/arclighttw/tinyreactors/managers/ResourceManager.java
@@ -1,6 +1,7 @@
 package com.arclighttw.tinyreactors.managers;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 
@@ -16,25 +17,72 @@ public class ResourceManager
 {
 	private static final Gson GSON = new Gson();
 	
-	public static JsonObject loadResource(ResourceLocation resource)
+	public static boolean hasResource(ResourceLocation resource)
 	{
 		try
 		{
-			BufferedReader reader = new BufferedReader(new InputStreamReader(Minecraft.getMinecraft().getResourceManager().getResource(resource).getInputStream()));
-			JsonElement element = GSON.fromJson(reader, JsonElement.class);
-			return element.getAsJsonObject();
+			InputStream input = Minecraft.getMinecraft().getResourceManager().getResource(resource).getInputStream();
+			
+			if(input != null)
+			{
+				input.close();
+				return true;
+			}
+			
+			input = ResourceManager.class.getClassLoader().getResourceAsStream("assets/" + resource.getResourceDomain() + "/" + resource.getResourcePath());
+			
+			if(input != null)
+			{
+				input.close();
+				return true;
+			}
 		}
 		catch(Exception e)
 		{
+		}
+		
+		return false;
+	}
+	
+	public static JsonObject loadResource(ResourceLocation resource)
+	{
+		BufferedReader reader = null;
+		
+		try
+		{
+			reader = new BufferedReader(new InputStreamReader(Minecraft.getMinecraft().getResourceManager().getResource(resource).getInputStream()));
+			JsonElement element = GSON.fromJson(reader, JsonElement.class);
+			JsonObject object = element.getAsJsonObject();
+			
+			if(reader != null)
+				reader.close();
+			
+			return object;
+		}
+		catch(Exception e)
+		{
+			if(reader != null)
+			{
+				try
+				{
+					reader.close();
+				}
+				catch(Exception e1)
+				{
+				}
+			}
+			
 			return new JsonObject();
 		}
 	}
 	
 	public static JsonObject[] loadResources(ResourceLocation resource)
 	{
+		BufferedReader reader = null;
+		
 		try
 		{
-			BufferedReader reader = new BufferedReader(new InputStreamReader(ResourceManager.class.getClassLoader().getResourceAsStream("assets/" + resource.getResourceDomain() + "/" + resource.getResourcePath())));
+			reader = new BufferedReader(new InputStreamReader(ResourceManager.class.getClassLoader().getResourceAsStream("assets/" + resource.getResourceDomain() + "/" + resource.getResourcePath())));
 			
 			List<String> files = Lists.newArrayList();
 			
@@ -47,10 +95,24 @@ public class ResourceManager
 			for(int i = 0; i < json.length; i++)
 				json[i] = loadResource(new ResourceLocation(resource.getResourceDomain(), resource.getResourcePath() + "/" + files.get(i)));
 			
+			if(reader != null)
+				reader.close();
+			
 			return json;
 		}
 		catch(Exception e)
 		{
+			if(reader != null)
+			{
+				try
+				{
+					reader.close();
+				}
+				catch(Exception e1)
+				{
+				}
+			}
+			
 			return new JsonObject[0];
 		}
 	}
