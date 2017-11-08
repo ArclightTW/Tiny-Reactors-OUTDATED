84a8b2e691f987f5f41c4cc06cd27ffe4366ff5a
Tom <tomawright95@gmail.com>
2017-11-08 07:56:57 +0000

    Created 'widgets' (Buttons for pages)

---
+++ src/main/java/com/arclighttw/tinyreactors/client/gui/manual/widgets/Widget.java
@@ -0,0 +1,19 @@
+package com.arclighttw.tinyreactors.client.gui.manual.widgets;
+
+import com.arclighttw.tinyreactors.client.gui.GuiTinyManual;
+
+public class Widget
+{
+	public void drawBackground(GuiTinyManual gui, int guiLeft, int guiTop, int mouseX, int mouseY)
+	{
+	}
+	
+	public void drawForeground(GuiTinyManual gui, int mouseX, int mouseY)
+	{
+	}
+	
+	public boolean mouseClicked(GuiTinyManual gui, int mouseX, int mouseY, int mouseButton)
+	{
+		return false;
+	}
+}
