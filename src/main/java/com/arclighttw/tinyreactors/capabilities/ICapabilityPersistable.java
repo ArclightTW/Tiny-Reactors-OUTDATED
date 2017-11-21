a3183c07e3eaf4c29d9cfd1b1412a3db25cc723f
ArclightTW <arclight@alstudios.co.uk>
2017-11-21 20:42:28 +0000

    v1.0 - Tom made a bad.

--- src/main/java/com/arclighttw/tinyreactors/capabilities/ICapabilityPersistable.java
+++
@@ -1,9 +0,0 @@
-package com.arclighttw.tinyreactors.capabilities;
-
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraftforge.common.util.INBTSerializable;
-
-public interface ICapabilityPersistable<T> extends INBTSerializable<NBTTagCompound>
-{
-	void set(T oldVersion);
-}
--- src/main/java/com/arclighttw/tinyreactors/properties/EnumRedstoneMode.java
+++
@@ -1,8 +0,0 @@
-package com.arclighttw.tinyreactors.properties;
-
-public enum EnumRedstoneMode
-{
-	IGNORE,
-	DISABLE_ON_REDSTONE,
-	ENABLE_ON_REDSTONE;
-}
